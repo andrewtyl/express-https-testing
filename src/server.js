a280e7cb0911cf44d4a195cf4034e6a148c966d5
Andrew Jessen-Tyler <andrewtyl.pro@gmail.com>
2020-04-14 13:59:27 -0600

    it works. SSL certs are expensive tho, and free ones aren't nice with most browsers.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const app = require('./app')
+const fs = require('fs')
+const https = require('https')
 const {PORT} = require('./config')
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app).listen(PORT, () => {
+  console.log(`Server listening at https://localhost:${PORT}`)
 })
